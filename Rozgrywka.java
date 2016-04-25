4519401c6d5e1e0313b33594c2906326ed6baa36
MateuszMa <mateusz.macalik@gmail.com>
2016-04-25 20:13:02 +0200

    Update Rozgrywka.java

--- Rozgrywka.java
+++ Rozgrywka.java
@@ -11,8 +11,18 @@ public class Rozgrywka {
 		RzutKoscmi player2= new RzutKoscmi();
 		player1.rzut();
 		player2.rzut();
+		//wywala cala tablice ale tylko jako string
 		System.out.println(Arrays.toString(player1.getWynik()));
 	  System.out.println(Arrays.toString(player2.getWynik()));
-	 }
+	 
+	 	//wywala po kolei kazda wartosc, zrobic metode?
+	 for (int i=0;i<5;i++){
+	    System.out.println(player1.getWynik()[i]+"    "+player2.getWynik()[i]);
+	 } 
+		
+		
+		
+	}
+	 
 	
 }
